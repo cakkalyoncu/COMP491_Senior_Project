f8a864fde6326c6069b1998da964b7b8e61317f5
Apple <apple@Apple-MacBook-Pro.local>
2019-11-12 13:48:31 +0300

    visual module can get color and location features

--- mysite/static/sketch.js
+++ mysite/static/sketch.js
@@ -1,73 +1,77 @@
-//Google Quick-draw Key: AIzaSyC1_soqtXV1mTyetVpJ4GglGD5RtXuFp4o
-//https://quickdrawfiles.appspot.com/drawing/cat?&key=AIzaSyC1_soqtXV1mTyetVpJ4GglGD5RtXuFp4o&isAnimated={isAnimated}&format=JSON
 const url1 = 'https://quickdrawfiles.appspot.com/drawing/'
 const url2 = '?&key=AIzaSyC1_soqtXV1mTyetVpJ4GglGD5RtXuFp4o&isAnimated=false&format=JSON'
+let bug; // Declare object
+let data;
+let bug2;
 
-let thingname;
-let drawingthing;
+function preload() {
+  let thingname = JSON.parse(document.getElementById('object').textContent);
+  //let url = url1+thingname+url2;
+  //alert(url);
+  data = loadJSON("https://quickdrawfiles.appspot.com/drawing/cat?&key=AIzaSyC1_soqtXV1mTyetVpJ4GglGD5RtXuFp4o&isAnimated=false&format=JSON");
+  data2 = loadJSON("https://quickdrawfiles.appspot.com/drawing/cat?&key=AIzaSyC1_soqtXV1mTyetVpJ4GglGD5RtXuFp4o&isAnimated=false&format=JSON");
+}
 
-let strokeIndex = 0;
-let index = 0;
-let cat;
-let prevx, prevy;
-let url; 
-let pick;
 
 function setup() {
-  createCanvas(600,600);
-  newDraw();
+  createCanvas(710, 400);
+  // Create object
+  let color1 =  color(0,0,255);
+  let color2 =  color(0,255,0);
+  bug = new DrawingObject(data,100,100,color1);
+  bug2 = new DrawingObject(data2,0,0,color2);
 }
-function pickathing(){
-  thingname = JSON.parse(document.getElementById('object').textContent);
-  pick = datatype[floor(random(344))];
-  //thingname = JSON.stringify(url1+pick.join()+url2);
-  //alert(typeof thingname); 
-}
-function newDraw() {
-  //alert(thingname);
-  //alert(typeof thingname);
-  //let url = url1+thingname+url2;
-  thingname = JSON.parse(document.getElementById('object').textContent);
-  let url = url1+thingname+url2;
-  console.log(url);
-  loadJSON(url, gotDraw);
-}
-
-function gotDraw(data) {
-  background(250);
-  cat = data.drawing;
-  //console.log(cat);
-  
-}
-
 
 function draw() {
-  if (cat) {
-    let x = cat[strokeIndex][0][index];
-    let y = cat[strokeIndex][1][index];
-    line_color = color(0, 0, 255);
-    stroke(line_color);
-    //strokeWeight(3);
-    if (prevx !== undefined) {
-      line(prevx, prevy, x, y);
-    }
-    index++;
-    if (index === cat[strokeIndex][0].length) {
-      strokeIndex++;
-      prevx = undefined;
-      prevy = undefined;
-      index = 0;
-      if (strokeIndex === cat.length) {
-        //console.log(strokeIndex);
-        cat = undefined;
-        strokeIndex = 0;
-        //setTimeout(newCat, 250);
-      }
-    } else {
-      prevx = x;
-      prevy = y;
-    }
-  }
+
+  bug.display();
+  bug2.display();
+
 }
 
+// Jitter class
+class DrawingObject {
+  constructor(data, location_X, location_Y, color) {
+    this.ready= 0;
+    //this.name =name;
+    this.index = 0;
+    this.strokeIndex = 0;
+    this.location_X = location_X;
+    this.location_Y = location_Y;
+    this.strokeArray = data.drawing;
+    this.diameter = random(10, 30);
+    this.speed = 1;
+    this.color= color;
+    // alert(this.color);
 
+
+  }
+
+
+    display() {
+//alert(this.strokeArray.length);
+  for(let i=0; i <= this.strokeArray[this.strokeIndex][0].length; i++){
+      let x = this.strokeArray[this.strokeIndex][0][this.index]+this.location_X ;
+      let y = this.strokeArray[this.strokeIndex][1][this.index]+this.location_Y;
+      stroke(this.color);
+      if (this.prevx !== undefined) {
+        line(this.prevx, this.prevy, x, y);
+      }
+      this.index++;
+      if (this.index === this.strokeArray[this.strokeIndex][0].length) {
+      this.strokeIndex++;
+      this.prevx = undefined;
+      this.prevy = undefined;
+      this.index = 0;
+      if (this.strokeIndex === this.strokeArray.length) {
+        this.strokeArray = undefined;
+        this.strokeIndex = 0;
+      }
+    } else {
+      this.prevx = x;
+      this.prevy = y;
+    }
+  }
+
+  }
+}
